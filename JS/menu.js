79299edba1403b578317658cecb447ba1cf19cbd
Sebastian Carlson <sebcar98@gmail.com>
2020-11-04 15:10:17 +0100

    har gjort så att menyn ändras beroende på fönster storlek

--- JS/menu.js
+++ JS/menu.js
@@ -1,20 +1,28 @@
-window.addEventListener('DOMContentLoaded', navigation);
-    
-    function navigation() {
-        let desktopNav = document.getElementById("desktop-nav");
-        let mobileNav = document.getElementById("mobile-nav");
-        let query = window.matchMedia('(min-width: 800px)');
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    let desktopNav = document.getElementById("desktop-nav");
+    let mobileNav = document.getElementById("mobile-nav");
+    let query = window.matchMedia('(min-width: 800px)');
+    if (query.matches) {
+        mobileNav.classList.add("hide");
+        desktopNav.classList.remove("hide");
+    }
+    if (!query.matches) {
+        desktopNav.classList.add("hide");
+        mobileNav.classList.remove("hide");
+    }
+    query.addEventListener("change", function (e) {
         if (query.matches) {
             mobileNav.classList.add("hide");
             desktopNav.classList.remove("hide");
-        } 
+        }
         if (!query.matches) {
             desktopNav.classList.add("hide");
             mobileNav.classList.remove("hide");
         }
-    }
-    
+    });
+});
+
+
 
 
 
