990791cf2f682cadf2b1f0db69c0c5216c89cc61
Sebastian Carlson <sebcar98@gmail.com>
2020-11-05 16:35:05 +0100

    har jobbat lite med första sidan

--- JS/menu.js
+++ JS/menu.js
@@ -1,22 +1,26 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let desktopNav = document.getElementById("desktop-nav");
     let mobileNav = document.getElementById("mobile-nav");
+    let icons = document.getElementById("icon-set");
     let query = window.matchMedia('(min-width: 800px)');
+
     if (query.matches) {
         mobileNav.classList.add("hide");
         desktopNav.classList.remove("hide");
-    }
-    if (!query.matches) {
+        icons.classList.remove("hide");
+    } else {
         desktopNav.classList.add("hide");
+        icons.classList.add("hide");
         mobileNav.classList.remove("hide");
     }
     query.addEventListener("change", function (e) {
         if (query.matches) {
             mobileNav.classList.add("hide");
             desktopNav.classList.remove("hide");
-        }
-        if (!query.matches) {
+            icons.classList.remove("hide");
+        } else {
             desktopNav.classList.add("hide");
+            icons.classList.add("hide");
             mobileNav.classList.remove("hide");
         }
     });
